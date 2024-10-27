47d2b4a05ff3b3fbd2af99402502de5cc7da835f
thirupathi4023 <thirupathi4023@gmail.com>
2024-10-27 18:32:59 +0530

    push

--- src/app/components/breakingnewsslider.tsx
+++ src/app/components/breakingnewsslider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import styles from './styles/breakingnewsslider.module.css';
@@ -37,11 +37,54 @@ const breakingNews = [
     content: "NASA's Perseverance Rover Discovers Ancient Organic Matter on Mars",
     time: "8 hours ago",
     author: "Kenneth Chang"
-  }
+  },
+  {
+    id: 5,
+    image: "https://picsum.photos/id/847/250/150",
+    title: "Breaking News",
+    content: "\"We Have a Deal\": Biden Agrees to Bipartisan Infrastructure Deal",
+    time: "2 hours ago",
+    author: "Jonathan Weisman"
+  },
+  {
+    id: 6,
+    image: "https://picsum.photos/id/847/250/150",
+    title: "Breaking News",
+    content: "\"We Have a Deal\": Biden Agrees to Bipartisan Infrastructure Deal",
+    time: "2 hours ago",
+    author: "Jonathan Weisman"
+  },
+  {
+    id: 7,
+    image: "https://picsum.photos/id/847/250/150",
+    title: "Breaking News",
+    content: "\"We Have a Deal\": Biden Agrees to Bipartisan Infrastructure Deal",
+    time: "2 hours ago",
+    author: "Jonathan Weisman"
+  },
+  {
+    id: 9,
+    image: "https://picsum.photos/id/847/250/150",
+    title: "Breaking News 9",
+    content: "\"We Have a Deal\": Biden Agrees to Bipartisan Infrastructure Deal",
+    time: "2 hours ago",
+    author: "Jonathan Weisman"
+  },
+  {
+    id: 9,
+    image: "https://picsum.photos/id/847/250/150",
+    title: "Breaking News 10",
+    content: "\"We Have a Deal\": Biden Agrees to Bipartisan Infrastructure Deal",
+    time: "2 hours ago",
+    author: "Jonathan Weisman"
+  },
 ];
 
 export default function BreakingNewsSlider() {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [touchStart, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
+  const sliderRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -59,9 +102,60 @@ export default function BreakingNewsSlider() {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % breakingNews.length);
   };
 
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    setTouchEnd(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStart - touchEnd > 75) {
+      handleNextSlide();
+    }
+
+    if (touchStart - touchEnd < -75) {
+      handlePrevSlide();
+    }
+  };
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setTouchStart(e.clientX);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (e.buttons === 1) {
+      setTouchEnd(e.clientX);
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (touchStart - touchEnd > 75) {
+      handleNextSlide();
+    }
+
+    if (touchStart - touchEnd < -75) {
+      handlePrevSlide();
+    }
+  };
+
+  const totalDots = 5; // You can change this to 4 if you prefer
+  const activeDotIndex = currentSlide % totalDots;
+
   return (
     <section className={styles.breakingNewsSlider}>
-      <div className={styles.sliderContainer}>
+      <div 
+        className={styles.sliderContainer}
+        ref={sliderRef}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+      >
         {breakingNews.map((news, index) => (
           <div
             key={news.id}
@@ -94,11 +188,11 @@ export default function BreakingNewsSlider() {
         <FaChevronRight />
       </button>
       <div className={styles.sliderDots}>
-        {breakingNews.map((_, index) => (
+        {[...Array(totalDots)].map((_, index) => (
           <span
             key={index}
-            className={`${styles.dot} ${index === currentSlide ? styles.active : ''}`}
-            onClick={() => setCurrentSlide(index)}
+            className={`${styles.dot} ${index === activeDotIndex ? styles.active : ''}`}
+            onClick={() => setCurrentSlide(index + Math.floor(currentSlide / totalDots) * totalDots)}
           ></span>
         ))}
       </div>
