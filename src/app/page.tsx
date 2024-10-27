ed6b8d9865962cca10db25ac39c22e3c1635b887
thirupathi4023 <thirupathi4023@gmail.com>
2024-10-27 13:14:59 +0530

    lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import BreakingNewsSlider from "./components/breakingnewsslider";
 import Categories from "./components/categories";
-import Footer from "./components/footer";
-import Header from "./components/header";
 import NewsList from "./components/newslist";
 import SearchBar from "./components/searchbar";
 
