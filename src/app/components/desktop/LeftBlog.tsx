13f26a082b118cceca7d5beac016f1ed6761792f
thirupathi4023 <thirupathi4023@gmail.com>
2024-12-03 23:44:55 +0530

    Lint

--- src/app/components/desktop/LeftBlog.tsx
+++ src/app/components/desktop/LeftBlog.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import ArticleCard from '../shared/ArticleCard';
 import { NewsItem } from '@/app/types/newsItem';
 
